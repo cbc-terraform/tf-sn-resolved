0be47e7142fd3316ccaae492411ed0e26369a131
Srividhya Varadhan <100423362+Srividhya0504@users.noreply.github.com>
2024-05-27 12:41:31 +0530

    Update secrets.tf

--- azure/azure_linux_docker_app_service/secrets.tf
+++ azure/azure_linux_docker_app_service/secrets.tf
@@ -6,3 +6,10 @@ resource "azurerm_key_vault_secret" "app_insights_instrumentation_key" {
 
   depends_on = [azurerm_key_vault_access_policy.principal]
 }
+resource "azurerm_key_vault_secret" "app_insights_instrumentation_key" {
+  key_vault_id = azurerm_key_vault.current.id
+  name         = "app-insights-key"
+  value        = azurerm_application_insights.current.instrumentation_key
+
+  depends_on = [azurerm_key_vault_access_policy.principal]
+}
